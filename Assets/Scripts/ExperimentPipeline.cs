92cf4d00bd1c6e523effccb6a51ac2cac52e0877
Sianuga <dzikiewicz.pawel@gmail.com>
2024-04-11 22:28:56 +0200

    Full experiment pipeline + testing scenes

---
+++ Assets/Scripts/ExperimentPipeline.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExperimentPipeline : MonoBehaviour
+{
+    public static ExperimentPipeline Instance { get; private set; }
+
+    [SerializeField] private Experiment experiment;
+    [SerializeField] private bool isDebugMode;
+    private int currentSceneIndex = 0;
+    private float currentTime = 0;
+    private bool hasStarted = false;
+    private List<SceneDuration> sceneDurations;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            sceneDurations = experiment.sceneDurations;
+        }
+        else
+        {
+            if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+
+
+    public void StartExperiment()
+    {
+        currentSceneIndex = 0;
+        hasStarted = true;
+        ResetTimer();
+        SceneManager.LoadScene(sceneDurations[currentSceneIndex].sceneName);
+    }
+
+    public void ResetTimer()
+    {
+        currentTime = 0;
+    }
+
+    public void StopTimer()
+    {
+        hasStarted = false;
+    }
+
+    public void NextScene()
+    {
+        currentSceneIndex++;
+        if (currentSceneIndex < sceneDurations.Count)
+        {
+            SceneManager.LoadScene(sceneDurations[currentSceneIndex].sceneName);
+        }
+        else
+        {
+            StopTimer();
+            Debug.Log("Experiment Finished");
+        }
+    }
+
+    public void Update()
+    {
+        if(hasStarted)
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime >= sceneDurations[currentSceneIndex].durationInSeconds)
+            {
+                NextScene();
+            }
+        }
+        
+    }
+
+    public void OnGUI()
+    {
+        if (hasStarted && isDebugMode)
+        {
+            GUILayout.Label("Current Scene: " + sceneDurations[currentSceneIndex].sceneName);
+            GUILayout.Label("Time Left: " + (sceneDurations[currentSceneIndex].durationInSeconds - currentTime));
+        }
+    }
+}
